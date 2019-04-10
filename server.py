705f4f122070b5a5f8a7ffedcdc1b260eb1e9f87
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2019-04-10 15:45:47 +1200

    Update server.py
    start api

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, send_from_directory, request, Markup
-import markdown, os, glob
+from flask import Flask, render_template, send_from_directory, request, Markup, Response
+import markdown, os, glob, json
 
 app = Flask(__name__)
 
@@ -18,7 +18,40 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-# shuld retune 5 usfull options only 
+@app.route('/api/<call>/')
+def api(call):
+    code = 200
+    key = request.args.get("key")
+    data = None 
+    if key != "":
+       # code = 403
+        ret = '{"error":{"code":403,"text":"Invalid Key"}}'
+    try:
+        data = json.load(request.data)
+    except:
+        code = 400
+        ret = '{"error":{"code":400,"text":"Invalid Json Post Data"}}'
+        
+    if call == 'SET' and code == 200:
+        # return code 201
+        # make new file
+        pass
+    elif call == 'DELETE' and code == 200:
+        pass# return code 200
+    elif call == 'UPDATE' and code == 200:
+        pass # return code 201
+    elif call == 'GET' and code == 200:
+        pass # return code 200
+    else:
+        code = 404
+        ret = '{"error":{"code":404,"text":"NOT A CALL"}}'
+    
+    resp = Response(response=ret,
+                    status=code,
+                    mimetype="application/json")
+    return resp
+
+
 @app.route('/searchin/<inp>')
 def menu(inp):
     files = glob.glob('markdown/'+inp+'*.md')
@@ -52,7 +85,6 @@ def search():
 @app.route('/p/<post>')
 def post(post):
     md = ""
-    code = 200
     try:
         f = open("markdown/"+post+".md", "r")
         md = f.read()
